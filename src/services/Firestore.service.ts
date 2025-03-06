ab023bb8fce1d355ae39374a7227e047d68dec89
djohoe28 <djohoe28@gmail.com>
2025-03-06 14:57:48 +0200

    (lint) Firestore service

--- src/services/Firestore.service.ts
+++ src/services/Firestore.service.ts
@@ -1,41 +1,6 @@
-// Import the functions you need from the SDKs you need
-import {
-	getFirestore,
-	collection,
-	doc,
-	getDoc,
-	setDoc,
-	onSnapshot,
-	onSnapshotsInSync,
-	serverTimestamp,
-} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import firebaseApp from "./Firebase.service";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Initialize Firebase
 
 const firestoreDb = getFirestore(firebaseApp);
 
-function getRoomDocumentReference(roomID: string) {
-	const rooms = collection(
-		firestoreDb,
-		import.meta.env.VITE_FIREBASE_ROOMS_COLLECTION_ID
-	);
-	const room = doc(rooms, roomID);
-	return room;
-}
-
-function mergeRoomToDocument(roomID: string, data: any) {
-	const room = getRoomDocumentReference(roomID);
-	return setDoc(room, data, { merge: true });
-}
-
-export {
-	firestoreDb,
-	getRoomDocumentReference,
-	mergeRoomToDocument,
-	onSnapshot,
-	serverTimestamp,
-};
-// TODO: Get Room DocumentReference as a context.
+export default firestoreDb;
