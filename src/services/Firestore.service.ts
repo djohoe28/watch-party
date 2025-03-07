11c59961665af655e3067342ad610472a3d66e43
djohoe28 <djohoe28@gmail.com>
2025-03-07 20:58:45 +0200

    Implemented Firestore Emulator connection for dev.

--- src/services/Firestore.service.ts
+++ src/services/Firestore.service.ts
@@ -1,6 +1,13 @@
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import firebaseApp from "./Firebase.service";
 
 const firestoreDb = getFirestore(firebaseApp); // TODO: Add Database ID?
-
+if (process.env.NODE_ENV === "development") {
+	const token = connectFirestoreEmulator(
+		firestoreDb,
+		import.meta.env.VITE_FIREBASE_FIRESTORE_EMULATOR_HOST,
+		import.meta.env.VITE_FIREBASE_FIRESTORE_EMULATOR_PORT
+	);
+	console.log("Firestore Emulator Token:", token);
+}
 export default firestoreDb;
