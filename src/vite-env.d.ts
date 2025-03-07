177095fe77b68ed3d06b662c91b8aa4e9e01d5cb
djohoe28 <djohoe28@gmail.com>
2025-03-07 20:58:36 +0200

    Declared Firestore Emulator environment variables.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,6 +9,9 @@ interface ImportMetaEnv {
 	readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
 	readonly VITE_FIREBASE_APP_ID: string;
 	readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+	// Local Development
+	readonly VITE_FIREBASE_FIRESTORE_EMULATOR_HOST: string;
+	readonly VITE_FIREBASE_FIRESTORE_EMULATOR_PORT: number;
 	// Environment Variables
 	readonly VITE_FIREBASE_ROOMS_COLLECTION_ID: string;
 }
