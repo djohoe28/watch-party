afa1faca000076627ee2cefa22d4f206d26d5872
djohoe28 <djohoe28@gmail.com>
2025-03-08 17:22:12 +0200

    (DRAFT) useRoomUsers & useRoomMessages

--- src/hooks/useRoomMessages.ts
+++ src/hooks/useRoomMessages.ts
@@ -1,37 +1,28 @@
-import {
-	collection,
-	doc,
-	onSnapshot,
-	orderBy,
-	query,
-} from "firebase/firestore";
-import firestoreDb from "../services/Firestore.service";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useState, useEffect } from "react";
-import RoomDocument from "../models/Firestore/RoomDocument.model";
 import { useRoomDocument } from "./useRoomDocument";
-import MessagesSubcollection from "../models/Firestore/MessagesSubcollection.model";
-import MessageDocument from "../models/Firestore/MessageDocument.model";
+import MessageDocument, {
+	MessageDocumentConverter,
+} from "../models/Firestore/MessageDocument.model";
 
 export const useRoomMessages = (roomId: string) => {
 	const {
-		roomDocumentRef,
-		roomData: roomDocumentData,
-		loading: roomDocumentLoading,
-		error: roomDocumentError,
+		roomRef: roomRef,
+		roomData: _,
+		loading: roomLoading,
+		error: roomError,
 	} = useRoomDocument(roomId);
 	const messagesCollectionRef = collection(
-		roomDocumentRef,
+		roomRef,
 		import.meta.env.VITE_FIREBASE_MESSAGES_SUBCOLLECTION_ID
-	); // TODO: Type this?
+	).withConverter(MessageDocumentConverter); // TODO: Check if MessageCollectionConverter is needed.
 	const messagesQuery = query(
 		messagesCollectionRef,
 		orderBy("sentAt", "asc")
 	);
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | Error | null>(null);
-	const [messagesData, setMessagesData] = useState<
-		(MessageDocument & { id: string })[] | null
-	>(null);
+	const [messages, setMessages] = useState<MessageDocument[] | null>(null); // TODO: Use AppModel
 
 	useEffect(() => {
 		if (!roomId) {
@@ -40,25 +31,38 @@ export const useRoomMessages = (roomId: string) => {
 			setLoading(false);
 			return;
 		}
+		if (roomLoading) {
+			setLoading(true);
+			return;
+		}
+		if (roomError) {
+			setError(roomError);
+			setLoading(false);
+			return;
+		}
 		setLoading(true);
 		try {
 			// Set up the real-time listener
 			const unsubscribe = onSnapshot(messagesQuery, (snapshot) => {
-				const messageList = snapshot.docs.map((doc) => ({
-					id: doc.id, // Document ID
-					...(doc.data() as MessageDocument), // Document Data
-				}));
-				setMessagesData(messageList);
+				snapshot.docChanges().map((change) => {
+					console.log(
+						import.meta.env.VITE_FIREBASE_MESSAGES_SUBCOLLECTION_ID,
+						change.type,
+						change.doc.data()
+					);
+				});
+				// TODO: Refactor to use docChanges() instead of re-mapping docs. (Reducer?)
+				const messageList = snapshot.docs.map((doc) => doc.data());
+				setMessages(messageList);
 				setLoading(false);
 				setError(null);
 			});
 			return () => unsubscribe();
 		} catch (err: Error | any) {
-			console.error("Error setting up chat listener:", err);
 			setError(`Failed to set up chat listener: ${err.message}`);
 			setLoading(false);
 		}
 	}, [roomId]);
 
-	return { messagesCollectionRef, messagesData, loading, error };
+	return { messages, loading, error };
 };
--- src/hooks/useRoomUsers.ts
+++ src/hooks/useRoomUsers.ts
@@ -1,34 +1,24 @@
-import {
-	collection,
-	doc,
-	onSnapshot,
-	orderBy,
-	query,
-} from "firebase/firestore";
-import firestoreDb from "../services/Firestore.service";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useState, useEffect } from "react";
-import RoomDocument from "../models/Firestore/RoomDocument.model";
 import { useRoomDocument } from "./useRoomDocument";
-import UsersSubcollection from "../models/Firestore/UsersSubcollection.model";
-import UserDocument from "../models/Firestore/UserDocument.model";
+import { UserDocumentConverter } from "../models/Firestore/UserDocument.model";
+import User from "../models/User.model";
 
 export const useRoomUsers = (roomId: string) => {
 	const {
-		roomDocumentRef,
-		roomData: roomDocumentData,
-		loading: roomDocumentLoading,
-		error: roomDocumentError,
+		roomRef: roomDocumentRef,
+		roomData: _,
+		loading: roomLoading,
+		error: roomError,
 	} = useRoomDocument(roomId);
-	const UsersCollectionRef = collection(
+	const usersCollectionRef = collection(
 		roomDocumentRef,
 		import.meta.env.VITE_FIREBASE_Users_SUBCOLLECTION_ID
-	); // TODO: Type this?
-	const UsersQuery = query(UsersCollectionRef, orderBy("sentAt", "asc"));
+	).withConverter(UserDocumentConverter);
+	const usersQuery = query(usersCollectionRef, orderBy("sentAt", "asc"));
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | Error | null>(null);
-	const [UsersData, setUsersData] = useState<
-		(UserDocument & { id: string })[] | null
-	>(null);
+	const [users, setUsers] = useState<User[] | null>(null); // TODO: UserDocument[]?
 
 	useEffect(() => {
 		if (!roomId) {
@@ -37,25 +27,33 @@ export const useRoomUsers = (roomId: string) => {
 			setLoading(false);
 			return;
 		}
+		if (roomLoading) {
+			setLoading(true);
+			return;
+		}
+		if (roomError) {
+			setError(roomError);
+			setLoading(false);
+			return;
+		}
 		setLoading(true);
 		try {
 			// Set up the real-time listener
-			const unsubscribe = onSnapshot(UsersQuery, (snapshot) => {
+			const unsubscribe = onSnapshot(usersQuery, (snapshot) => {
 				const userList = snapshot.docs.map((doc) => ({
-					id: doc.id, // Document ID
-					...(doc.data() as UserDocument), // Document Data
+					...doc.data(), // Document Data
+					id: doc.id, // Document ID // TODO: Override ID in UserDocument model?
 				}));
-				setUsersData(userList);
+				setUsers(userList);
 				setLoading(false);
 				setError(null);
 			});
 			return () => unsubscribe();
 		} catch (err: Error | any) {
-			console.error("Error setting up chat listener:", err);
 			setError(`Failed to set up chat listener: ${err.message}`);
 			setLoading(false);
 		}
 	}, [roomId]);
 
-	return { UsersCollectionRef, UsersData, loading, error };
+	return { users, loading, error };
 };
