ad21b004ac5e48ebcc314e9ff73da53ec83ccece
djohoe28 <djohoe28@gmail.com>
2025-03-15 18:30:16 +0200

    Added React Contexts (& Providers) - MessagesContext - RoomIdContext - UserIdContext

--- src/contexts/MessagesContext.tsx
+++ src/contexts/MessagesContext.tsx
@@ -3,6 +3,7 @@ import MessageDocument from "../models/Firestore/MessageDocument.model";
 import { useRoomMessagesQuery } from "../hooks/useRoomMessagesQuery";
 import { FirestoreQueryContextType } from "../types/FirestoreContextType";
 
+// TODO: Is Messages used outside of RoomContext? Of MessageList?
 export const MessagesContext = createContext<FirestoreQueryContextType<MessageDocument, MessageDocument> | null>(null);
 
 export const MessagesContextProvider = ({ children, roomId }: { children: ReactNode, roomId: string }) => {
---
+++ src/contexts/RoomIdContext.tsx
@@ -0,0 +1,10 @@
+import { createContext, ReactNode, useState } from "react";
+
+const DEFAULT_ROOM_ID = "RoomIDGoesHere";
+export const RoomIdContext = createContext<string>(DEFAULT_ROOM_ID);
+
+export const RoomIdContextProvider = ({ children }: { children: ReactNode }) => {
+	const ROOM_ID_REGEX = /\/room\/(.*)$/i; // TODO: Check if this is URL safe?
+	const [roomId, setRoomId] = useState<string>(ROOM_ID_REGEX.exec(window.location.href)?.[0] || DEFAULT_ROOM_ID);
+	return <RoomIdContext.Provider value={roomId}>{children}</RoomIdContext.Provider>
+};
