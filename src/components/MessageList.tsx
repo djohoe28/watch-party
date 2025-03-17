967367ab3d389dacb2d1b4ee6fe4c77c52479f9b
djohoe28 <djohoe28@gmail.com>
2025-03-17 19:13:20 +0200

    Read User ID context in MessageList to pass to MessageItem.

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -2,10 +2,12 @@ import { MessageItem } from "./MessageItem";
 import { useRoomMessagesQuery } from "../hooks/useRoomMessagesQuery";
 import { useContext } from "react";
 import { RoomIdContext } from "../contexts/RoomIdContext";
+import { UserIdContext } from "../contexts/UserIdContext";
 
 export const MessageList = () => {
 	// Properties
 	const roomId = useContext(RoomIdContext);
+	const userId = useContext(UserIdContext);
 	const { data, loading, error } = useRoomMessagesQuery(roomId); // TODO: Replace with useContext.
 	return (
 		<div>
@@ -13,7 +15,7 @@ export const MessageList = () => {
 			{error && <div>Error: {error.toString()}</div>}
 			{data?.map((message) => (
 				// TODO: message.id is only applied in useRoomMessagesQuery.
-				<MessageItem key={message.id} message={message} />
+				<MessageItem key={message.id} message={message} userId={userId} />
 			))}
 		</div>
 	);
