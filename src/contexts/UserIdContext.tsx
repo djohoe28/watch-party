526a6810ad7741973def6e78552558cbb2fac716
djohoe28 <djohoe28@gmail.com>
2025-03-17 19:00:51 +0200

    Updated UserIdContextProvider with useAuth hook.

--- src/contexts/UserIdContext.tsx
+++ src/contexts/UserIdContext.tsx
@@ -1,9 +1,19 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
 
-export const UserIdContext = createContext<string>("UserIDGoesHere");
+export const UserIdContext = createContext<string | null>(null);
 
 export const UserIdContextProvider = ({ children }: { children: ReactNode }) => {
 	// TODO: https://firebase.google.com/docs/auth/web/anonymous-auth
-	const [userId, setUserId] = useState<string>("UserIDGoesHere"); // TODO: Load from Memory?
+	const [userId, setUserId] = useState<string | null>(null); // TODO: Load from Memory?
+	// TODO: Should `loading` be exported?
+	const [loading, setLoading] = useState<boolean>(true);
+	useEffect(() => {
+		setLoading(true);
+		useAuth().then((credential) => {
+			setUserId(credential.user.uid);
+			setLoading(false);
+		});
+	}, [])
 	return <UserIdContext.Provider value={userId}>{children}</UserIdContext.Provider>
 }
