885fef884f4b629a5702a6835772f7f02afce478
djohoe28 <djohoe28@gmail.com>
2025-03-17 19:28:49 +0200

    (DRAFT) Added user ID display to Room

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -4,13 +4,17 @@ import { MessageList } from "./MessageList";
 import { useRoomDocument } from "../hooks/useRoomDocument";
 import { RoomIdContext } from "../contexts/RoomIdContext";
 import { RoomContextProvider } from "../contexts/RoomContext";
+import { useContext } from "react";
+import { UserIdContext } from "../contexts/UserIdContext";
 
 export const Room = ({ roomId }: { roomId: string }) => {
-	const roomDocument = useRoomDocument(roomId);
+	const roomDocument = useRoomDocument(roomId); // TODO: Use Context?
+	const userId = useContext(UserIdContext); // TODO: Use User instead of UserID?
 	return <RoomIdContext.Provider value={roomId}>
 		<RoomContextProvider roomId={roomId}>
 			<Typography variant="h5" component="h2" sx={{ mb: 2 }}>
 				{roomDocument.loading ? <Skeleton /> : roomDocument.error ? roomDocument.error.toString() : roomDocument.data?.title}
+				{userId && ` (${userId})`}
 			</Typography>
 			<MessageList />
 			<MessageField />
