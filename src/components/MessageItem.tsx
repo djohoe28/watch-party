6507280c205fe23a647aabd721f2bbec6979f2fc
djohoe28 <djohoe28@gmail.com>
2025-03-17 19:14:51 +0200

    (minor) added `| null` to `userId` in `MessageItem` for loading

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,6 +1,6 @@
 import MessageDocument from "../models/Firestore/MessageDocument.model";
 
-export const MessageItem = ({ message, userId }: { message: MessageDocument, userId?: string }) => {
+export const MessageItem = ({ message, userId }: { message: MessageDocument, userId?: string | null }) => {
 	// TODO: if message.senderId == userId
 	return <div className={message.senderId === userId ? "sender-self" : "sender-other"}>{message.content}</div>;
 };
