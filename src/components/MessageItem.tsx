bcdb58749a35fce27e033849837b6657bd4ec708
djohoe28 <djohoe28@gmail.com>
2025-03-17 19:12:31 +0200

    Read User ID context in MessageList to pass to MessageItem.

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,6 +1,6 @@
 import MessageDocument from "../models/Firestore/MessageDocument.model";
 
-export const MessageItem = ({ message }: { message: MessageDocument }) => {
+export const MessageItem = ({ message, userId }: { message: MessageDocument, userId?: string }) => {
 	// TODO: if message.senderId == userId
-	return <div>{message.content}</div>;
+	return <div className={message.senderId === userId ? "sender-self" : "sender-other"}>{message.content}</div>;
 };
