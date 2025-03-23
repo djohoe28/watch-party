25a046c6bf0013dbb86d33edfd98d0767b8e81f3
djohoe28 <djohoe28@gmail.com>
2025-03-23 17:43:40 +0200

    Services: process.env.NODE_ENV => import.meta.env.DEV

--- src/services/Auth.service.ts
+++ src/services/Auth.service.ts
@@ -1,7 +1,8 @@
 import { getAuth, connectAuthEmulator } from "firebase/auth";
+import firebaseApp from "./Firebase.service";
 
-const auth = getAuth();
-if (process.env.NODE_ENV === "development") {
+const auth = getAuth(firebaseApp);
+if (import.meta.env.DEV) {
 	const token = connectAuthEmulator(
 		auth,
 		import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST
--- src/services/Firestore.service.ts
+++ src/services/Firestore.service.ts
@@ -2,7 +2,7 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import firebaseApp from "./Firebase.service";
 
 const firestoreDb = getFirestore(firebaseApp); // TODO: Add Database ID?
-if (process.env.NODE_ENV === "development") {
+if (import.meta.env.DEV) {
 	const token = connectFirestoreEmulator(
 		firestoreDb,
 		import.meta.env.VITE_FIREBASE_FIRESTORE_EMULATOR_HOST,
