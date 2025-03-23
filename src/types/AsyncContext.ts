69a30e03ba0c5cfba8995dcd78b337f484fd9ca0
djohoe28 <djohoe28@gmail.com>
2025-03-23 17:45:21 +0200

    Added interface AsyncContext<T>

---
+++ src/types/AsyncContext.ts
@@ -0,0 +1,11 @@
+export type ErrorType = Error | string | null;
+
+export interface AsyncContext<T> {
+	payload: T | null;
+	loading: boolean;
+	error: ErrorType;
+}
+
+export const createDefaultContext = <T>(): AsyncContext<T> => {
+	return { payload: null, loading: true, error: null };
+};
