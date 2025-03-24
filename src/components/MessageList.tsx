37596015c8422ad4dda537c862ed5b863dde0eb7
djohoe28 <djohoe28@gmail.com>
2025-03-24 18:05:50 +0200

    MessageItem, MessageList upgraded; - MessageItem - - timestampDisplayString - - userName prop - MessageList - - usersContext (for userName prop)

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,24 +1,30 @@
 import { MessageItem } from "./MessageItem";
 import { useRoomMessagesQuery } from "../hooks/useRoomMessagesQuery";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { RoomContext } from "../contexts/RoomContext";
 import UserContext from "../contexts/UserContext";
+import { UsersContext } from "../contexts/UsersContext";
 
 export const MessageList = () => {
 	// Properties
 	const roomContext = useContext(RoomContext);
-	const userContext = useContext(UserContext);
 	if (!roomContext) {
 		return <div>No Room ID provided</div>;
 	}
+	const userContext = useContext(UserContext);
+	const usersContext = useContext(UsersContext);
 	const { data, loading, error } = useRoomMessagesQuery(roomContext.payload); // TODO: Replace with useContext.
 	return (
 		<div>
 			{loading && <div>Loading...</div>}
 			{error && <div>Error: {error.toString()}</div>}
 			{data?.map((message) => (
-				// TODO: message.id is only applied in useRoomMessagesQuery.
-				<MessageItem key={message.id} message={message} userId={userContext.payload?.uid} />
+				// TODO: message.id is only applied in useRoomMessagesQuery; Use dedicated Converter? Add ID to GenericConverter?
+				<MessageItem
+					key={message.id}
+					message={message}
+					userId={userContext.payload?.uid}
+					userName={usersContext?.data?.find((user) => user.id === message.senderId)?.name} />
 			))}
 		</div>
 	);
