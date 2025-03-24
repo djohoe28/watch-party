d506200f219c832d0d70916f4b9188e5f73f2823
djohoe28 <djohoe28@gmail.com>
2025-03-24 18:02:44 +0200

    UsersContextProvider added to Room.

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,10 +1,11 @@
-import { Typography, Skeleton } from "@mui/material";
+import { Typography, Skeleton, Stack } from "@mui/material";
 import { MessageField } from "./MessageField";
 import { MessageList } from "./MessageList";
 import { useRoomDocument } from "../hooks/useRoomDocument";
 import { RoomContextProvider } from "../contexts/RoomContext";
 import { useContext } from "react";
 import UserContext from "../contexts/UserContext";
+import { UsersContextProvider } from "../contexts/UsersContext";
 
 export const Room = ({ roomId }: { roomId: string }) => {
 	const roomDocument = useRoomDocument(roomId); // TODO: Use Context?
@@ -15,7 +16,13 @@ export const Room = ({ roomId }: { roomId: string }) => {
 			{roomDocument.loading ? <Skeleton /> : roomDocument.error ? roomDocument.error.toString() : roomDocument.data?.title}
 			{userContext.payload && ` (${userContext.payload?.uid})`}
 		</Typography>
-		<MessageList />
-		<MessageField />
+		<Stack direction="row" spacing={2}>
+			<Stack direction="column" spacing={2}>
+				<UsersContextProvider roomRef={roomDocument.payload}>
+					<MessageList />
+				</UsersContextProvider>
+				<MessageField />
+			</Stack>
+		</Stack>
 	</RoomContextProvider>
 };
