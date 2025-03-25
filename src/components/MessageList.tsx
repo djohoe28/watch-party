c0955a427503a754f0b7fe178a75611903859645
djohoe28 <djohoe28@gmail.com>
2025-03-25 18:46:38 +0200

    Refactored MessageList & MessageItem as List & ListItem; - MessageList div => List - MessageItem div => ListItem - Refactored userId string prop to isSelf boolean prop. - Refactored userName string prop to userModel prop. - MessageList returns error div, loading Skeleton.

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,9 +1,10 @@
 import { MessageItem } from "./MessageItem";
 import { useRoomMessagesQuery } from "../hooks/useRoomMessagesQuery";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { RoomContext } from "../contexts/RoomContext";
 import UserContext from "../contexts/UserContext";
 import { UsersContext } from "../contexts/UsersContext";
+import { List, Skeleton } from "@mui/material";
 
 export const MessageList = () => {
 	// Properties
@@ -14,18 +15,17 @@ export const MessageList = () => {
 	const userContext = useContext(UserContext);
 	const usersContext = useContext(UsersContext);
 	const { data, loading, error } = useRoomMessagesQuery(roomContext.payload); // TODO: Replace with useContext.
+	if (error) return <div>Error: {error.toString()}</div>;
 	return (
-		<div>
-			{loading && <div>Loading...</div>}
-			{error && <div>Error: {error.toString()}</div>}
-			{data?.map((message) => (
-				// TODO: message.id is only applied in useRoomMessagesQuery; Use dedicated Converter? Add ID to GenericConverter?
+		<List sx={{ width: "100%", bgcolor: "background.paper" }}>
+			{loading ? <Skeleton /> : data?.map((message) => (
 				<MessageItem
 					key={message.id}
 					message={message}
-					userId={userContext.payload?.uid}
-					userName={usersContext?.data?.find((user) => user.id === message.senderId)?.name} />
+					isSelf={userContext.payload?.uid === message.senderId}
+					userModel={usersContext?.data?.find((user) => user.id === message.senderId)}
+				/>
 			))}
-		</div>
+		</List>
 	);
 };
