8e5674ab1c37a495bc062c31d2b9d47900727397
djohoe28 <djohoe28@gmail.com>
2025-03-25 18:15:28 +0200

    UsersContext type refactored UserDocument => UserModel.

--- src/contexts/UsersContext.tsx
+++ src/contexts/UsersContext.tsx
@@ -3,8 +3,9 @@ import { FirestoreCollectionContextType } from "../types/FirestoreContextType";
 import UserDocument from "../models/Firestore/UserDocument.model";
 import { useRoomUsersCollection } from "../hooks/useRoomUsersCollection";
 import { DocumentReference } from "firebase/firestore";
+import UserModel from "../models/User.model";
 
-export const UsersContext = createContext<FirestoreCollectionContextType<UserDocument, UserDocument> | null>(null);
+export const UsersContext = createContext<FirestoreCollectionContextType<UserModel, UserDocument> | null>(null);
 
 export const UsersContextProvider = ({ children, roomRef }: { children: ReactNode, roomRef: DocumentReference }) => {
 	const usersContext = useRoomUsersCollection(roomRef);
