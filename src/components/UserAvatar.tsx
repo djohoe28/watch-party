e3cbffaa8fa2c0902b3cbbebc1fd5d97970d4ca1
djohoe28 <djohoe28@gmail.com>
2025-03-25 18:42:15 +0200

    Refactored UserAvatar to fallback on user.id.

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -3,5 +3,5 @@ import UserModel from "../models/User.model";
 import { stringToColor, stringToInitials } from "../utils/String.utils";
 
 export default function UserAvatar({ user }: { user: UserModel }) {
-	return <Avatar sx={{ bgcolor: user.color ?? stringToColor(user.id) }}>{stringToInitials(user.name)}</Avatar>;
+	return <Avatar sx={{ bgcolor: user.color ?? stringToColor(user.id) }}>{user.name ? stringToInitials(user.name) : user.id}</Avatar>;
 }
