02ce537a49a1187304f8925531a6fba59e86f098
djohoe28 <djohoe28@gmail.com>
2025-03-27 20:44:23 +0200

    Added (partial) Data Validation to useUserSettings.

--- src/hooks/useUserSettings.ts
+++ src/hooks/useUserSettings.ts
@@ -1,7 +1,8 @@
-import { setDoc } from "firebase/firestore";
+import { deleteField, FieldValue, setDoc } from "firebase/firestore";
 import { useCallback, useState } from "react";
 import { RoomUserContextType } from "../contexts/RoomUserContext";
 import UserModel from "../models/User.model";
+import { isColor, stringToColor } from "../utils/String.utils";
 
 export const useUserSettings = (roomUserContext: RoomUserContextType) => {
 	// TODO: Reference is null *only* when roomId is invalid; Leverage this to reuse hooks!
@@ -13,11 +14,13 @@ export const useUserSettings = (roomUserContext: RoomUserContextType) => {
 	const ref = roomUserContext?.payload;
 	// Callbacks
 	const sendUserSettings = useCallback(
-		async (settings: Partial<UserModel>) => {
-			// TODO: Add senderName?
+		async (
+			settings: Partial<Omit<UserModel, "id">>,
+			merge: boolean = true
+		) => {
 			if (!roomUserContext) {
 				// TODO: Check if Room ID is URL safe?
-				setError("No (Room) User Document provided");
+				setError("No (Room) User Document context provided");
 				setLoading(false);
 				return;
 			}
@@ -28,7 +31,17 @@ export const useUserSettings = (roomUserContext: RoomUserContextType) => {
 			}
 			setSending(true);
 			try {
-				await setDoc(ref, settings, { merge: true });
+				// Data Validation
+				const isInvalidColor =
+					settings.color && !isColor(settings.color);
+				const isDefaultColor = roomUserContext.data
+					? settings.color === stringToColor(roomUserContext.data?.id)
+					: false;
+				if (!settings.name?.trim()) delete settings.name;
+				if (isInvalidColor || isDefaultColor) delete settings.color;
+				// TODO: Prevent if settings is same as server?
+				// Send
+				await setDoc(ref, settings, { merge: merge });
 				setSending(false);
 				setError(null);
 			} catch (err: Error | any) {
