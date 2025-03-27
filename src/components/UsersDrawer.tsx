4da2c1d31c349a6eef7d3e48c2dccc331821373a
djohoe28 <djohoe28@gmail.com>
2025-03-27 20:48:21 +0200

    Added tooltip to UsersList IconButton.

--- src/components/UsersDrawer.tsx
+++ src/components/UsersDrawer.tsx
@@ -1,21 +1,24 @@
-import { Drawer, IconButton } from "@mui/material"
+import { Drawer, IconButton, Tooltip } from "@mui/material"
 import { Fragment, useState } from "react";
 import PeopleIcon from '@mui/icons-material/People';
 import CloseIcon from '@mui/icons-material/Close';
 import UsersList from "./UsersList";
 
 export const UsersDrawer = () => {
+	// States
 	const [open, setOpen] = useState(true);
 	const handleToggle = () => setOpen(!open);
 	const handleClose = () => setOpen(false);
 	return <Fragment>
-		<IconButton
-			onClick={handleToggle}
-			sx={{ borderRadius: 1 }}
-			aria-label="Toggle Users List"
-		>
-			<PeopleIcon />
-		</IconButton>
+		<Tooltip title="Users List">
+			<IconButton
+				onClick={handleToggle}
+				sx={{ borderRadius: 1, width: '100%' }}
+				aria-label="Toggle Users List"
+			>
+				<PeopleIcon />
+			</IconButton>
+		</Tooltip>
 		<Drawer variant="persistent" anchor="right" open={open}>
 			<IconButton
 				onClick={handleClose}
