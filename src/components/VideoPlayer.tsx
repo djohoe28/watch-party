45b218a8aa7a28ecdd619687210c39d440a5d685
djohoe28 <djohoe28@gmail.com>
2025-03-30 18:23:46 +0300

    (DRAFT) Added buttons & sliders for VideoPlayer.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,17 +1,26 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { RoomContext } from "../contexts/RoomContext";
-import { TimestampConverter } from "../models/Firestore/Timestamp.model";
+import { IconButton, Slider, Stack } from "@mui/material";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import { toTimespanString } from "../models/Firestore/Timestamp.model";
 
 export const VideoPlayer = () => {
 	const roomContext = useContext(RoomContext);
 	const videoRef = useRef<HTMLVideoElement>(null);
 	const [isLoaded, setIsLoaded] = useState(videoRef.current?.readyState === HTMLMediaElement.HAVE_ENOUGH_DATA);
 	const [isPaused, setIsPaused] = useState<boolean | undefined>(videoRef.current?.paused);
-	const [currentTime, setCurrentTime] = useState<number | undefined>(videoRef.current?.currentTime);
-	const [maxDelta, setMaxDelta] = useState<number>(0);
+	const [currentTime, setCurrentTime] = useState<number | undefined>(videoRef.current?.currentTime); // NOTE: seconds
+	const [duration, setDuration] = useState<number | undefined>(videoRef.current?.duration);
+	const [volume, setVolume] = useState<number>(0); // NOTE: 0-1
+	const [maxDelta, setMaxDelta] = useState<number>(0); // NOTE: ms
+	const showHours = useMemo(() => (duration && duration > 3600) || false, [duration]);
 	// Un/Mount Event Listeners
 	useEffect(() => {
 		if (videoRef.current) {
+			videoRef.current.volume
 			videoRef.current.onloadeddata = () => setIsLoaded(true);
 			videoRef.current.onpause = () => setIsPaused(true);
 			videoRef.current.onplay = () => setIsPaused(false);
@@ -27,12 +36,16 @@ export const VideoPlayer = () => {
 		}
 	}, [videoRef.current]);
 	useEffect(() => {
+		setDuration(videoRef.current?.duration);
+	}, [videoRef.current?.duration]);
+	useEffect(() => {
 		if (roomContext?.data?.media && videoRef.current) {
 			const mediaState = roomContext?.data?.media;
 			const video = videoRef.current;
 			console.log("mediaState", mediaState);
-			const deltaTime = Date.now() - TimestampConverter.fromFirestore(mediaState.lastUpdated).getTime();
+			const deltaTime = Date.now() - mediaState.lastUpdated.toDate().getTime();
 			if (mediaState.src !== video.src) {
+				console.log("mediaState.src !== video.src");
 				video.src = mediaState.src; // TODO: Async! Do this without triggering state / database updates!
 				setIsLoaded(false);
 				video.load(); // TODO: Wait for this to finish before handling other state updates!
@@ -41,16 +54,34 @@ export const VideoPlayer = () => {
 			// TODO: Leverage isLoaded, isPaused, etc. to handle video's async updates.
 			// When isLoaded is false, we should wait for it to be true before handling other state updates.
 			if (mediaState.isPaused === true && video.paused === false) {
+				console.log("mediaState.isPaused === true && video.paused === false");
 				video.pause(); // TODO: Do this without triggering state / database updates!
 			}
-			if (!mediaState.isPaused && Math.abs(mediaState.currentTime - video.currentTime) > maxDelta) {
+			if ((mediaState.isPaused === false && Math.abs(mediaState.currentTime - video.currentTime) > maxDelta) || mediaState.isPaused === true) {
+				// NOTE: If paused, or if playing & currentTime is too far away, update currentTime.
+				console.log("Math.abs(mediaState.currentTime - video.currentTime) > maxDelta");
 				video.currentTime = mediaState.currentTime + (mediaState.isPaused ? 0 : deltaTime / 1000);
 			}
 			if (mediaState.isPaused === false && video.paused === true) {
+				console.log("mediaState.isPaused === false && video.paused === true");
 				// setIsPaused(false);
 				video.play(); // TODO: await? Do this without triggering state / database updates!
 			}
 		}
 	}, [roomContext?.data?.media, videoRef.current]); // TODO: Add isLoaded etc. as dependencies?
-	return <video ref={videoRef} style={{ maxWidth: "50%" }} autoPlay={true} controls={true} muted={true}></video>
+	return <Stack direction="column">
+		<video ref={videoRef} style={{ maxWidth: "50%" }} autoPlay={true} controls={true} muted={true}></video>
+		<Stack direction="row" spacing={2} alignItems="center">
+			<IconButton onClick={() => videoRef.current?.play()}><PlayArrowIcon /></IconButton>
+			<IconButton onClick={() => videoRef.current?.pause()}><PauseIcon /></IconButton>
+			<VolumeUpIcon />
+			<Slider value={volume} min={0} max={1} onChange={(e, value) => { if (videoRef.current) videoRef.current.volume = value as number }} />
+		</Stack>
+		<Stack direction="row" spacing={2} alignItems="center">
+			{/** TODO: Move to external component to avoid re-renders? */}
+			<span>{toTimespanString(videoRef.current?.currentTime || 0, showHours)}</span>
+			<Slider value={currentTime} min={0} max={videoRef.current?.duration} onChange={(e, value) => { if (videoRef.current?.currentTime) videoRef.current.currentTime = value as number }} />
+			<span>{toTimespanString(videoRef.current?.duration || 0, showHours)}</span>
+		</Stack>
+	</Stack>
 };
