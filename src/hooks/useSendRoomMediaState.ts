1c3731178f8a7c05665c0674039b60edb94d6e44
djohoe28 <djohoe28@gmail.com>
2025-04-02 12:21:55 +0300

    (DRAFT) Implemented useSendRoomMediaState.

---
+++ src/hooks/useSendRoomMediaState.ts
@@ -0,0 +1,51 @@
+import { setDoc } from "firebase/firestore";
+import { useCallback, useState } from "react";
+import { RoomContextType } from "../contexts/RoomContext";
+import MediaStateMap from "../models/Firestore/MediaStateMap.model";
+import { ErrorType } from "../types/AsyncContext";
+
+export const useSendRoomMediaState = (roomContext: RoomContextType | null) => {
+	// TODO: Reference is null *only* when roomId is invalid; Leverage this to reuse hooks!
+	// States
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<ErrorType>(null);
+	const [sending, setSending] = useState<boolean>(false);
+	// Properties
+	const ref = roomContext?.payload;
+	// Callbacks
+	const sendRoomMediaState = useCallback(
+		(settings: MediaStateMap) => {
+			if (!roomContext) {
+				// TODO: Check if Room ID is URL safe?
+				setError("No Room Document context provided");
+				setLoading(false);
+				return;
+			}
+			if (!ref) {
+				setError("Failed to get Room Document reference");
+				setLoading(false);
+				return;
+			}
+			setSending(true);
+			// Data Validation
+			// TODO: Enforce settings to include *only* MediaStateMap properties?
+			// TODO: Prevent if settings is same as server?
+			// Send
+			setDoc(
+				ref,
+				{ media: { ...roomContext.data?.media, ...settings } },
+				{ merge: true }
+			)
+				.then(() => {
+					setSending(false);
+					setError(null);
+				})
+				.catch((err) => {
+					setSending(false);
+					setError(err);
+				});
+		},
+		[roomContext, ref]
+	);
+	return { sendUserSettings: sendRoomMediaState, sending, loading, error };
+};
