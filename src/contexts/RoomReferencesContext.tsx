9174ce11f8231182648392278784785aafec4992
djohoe28 <djohoe28@gmail.com>
2025-04-07 14:41:27 +0300

    Added RoomReferencesContext.

---
+++ src/contexts/RoomReferencesContext.tsx
@@ -0,0 +1,9 @@
+import { createContext, ReactNode } from "react";
+import { RoomReferences, useRoomReferences } from "../hooks/useRoomReferences";
+
+export const RoomReferencesContext = createContext<RoomReferences | null>(null);
+
+export const RoomReferencesContextProvider = ({ children, roomId, userId }: { children: ReactNode, roomId: string, userId?: string }) => {
+	const roomDocumentReference = useRoomReferences(roomId, userId);
+	return <RoomReferencesContext.Provider value={roomDocumentReference}>{children}</RoomReferencesContext.Provider>
+};
