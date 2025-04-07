53eabbc0f3d5bc93a93d274feff60a0e6b7ed017
djohoe28 <djohoe28@gmail.com>
2025-04-07 18:57:34 +0300

    Added loading/error handling to MembersContext.

--- src/contexts/MembersContext.tsx
+++ src/contexts/MembersContext.tsx
@@ -2,11 +2,15 @@ import { createContext, ReactNode } from "react";
 import MemberModel from "../models/Member.model";
 import { MembersCollectionReference } from "../models/Firestore/MembersCollection.model";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import { Alert, Skeleton } from "@mui/material";
 
 export const MembersContext = createContext<MemberModel[] | null | undefined>(null); // LINT: undefined?
 
 export const MembersContextProvider = ({ children, membersRef }: { children: ReactNode, membersRef: MembersCollectionReference | null | undefined }) => {
 	const [collectionData, collectionLoading, collectionError, _] = useCollectionData(membersRef);
-	// LINT: Leverage loading, error?
-	return <MembersContext.Provider value={collectionData}>{children}</MembersContext.Provider>
+	
+	if (collectionError) return <Alert severity="error">{collectionError.toString()}</Alert>;
+	return collectionLoading
+		? <Skeleton />
+		: <MembersContext.Provider value={collectionData}>{children}</MembersContext.Provider>
 };
