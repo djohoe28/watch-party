e10af549a4ce789fa9df1f256da0fd262e5ed834
djohoe28 <djohoe28@gmail.com>
2025-04-07 17:29:36 +0300

    (lint) DrawerWithToggle formatted.

--- src/components/DrawerWithToggle.tsx
+++ src/components/DrawerWithToggle.tsx
@@ -3,42 +3,42 @@ import { Fragment, ReactNode, useCallback, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 
 interface DrawerWithToggleProps {
-    icon: ReactNode;
-    tooltip: string;
-    anchor: "left" | "right";
-    children: ReactNode;
-    ariaLabel: string;
+	icon: ReactNode;
+	tooltip: string;
+	anchor: "left" | "right";
+	children: ReactNode;
+	ariaLabel: string;
 }
 
 export const DrawerWithToggle = ({ icon, tooltip, anchor, children, ariaLabel }: DrawerWithToggleProps) => {
-    // State
-    const [open, setOpen] = useState(false);
+	// State
+	const [open, setOpen] = useState(false);
 
-    // Callbacks
-    const handleToggle = useCallback(() => setOpen((open) => !open), []);
-    const handleClose = useCallback(() => setOpen(false), []);
+	// Callbacks
+	const handleToggle = useCallback(() => setOpen((open) => !open), []);
+	const handleClose = useCallback(() => setOpen(false), []);
 
-    return (
-        <Fragment>
-            <Tooltip title={tooltip}>
-                <IconButton
-                    onClick={handleToggle}
-                    sx={{ borderRadius: 1, width: "100%" }}
-                    aria-label={ariaLabel}
-                >
-                    {icon}
-                </IconButton>
-            </Tooltip>
-            <Drawer variant="persistent" anchor={anchor} open={open}>
-                <IconButton
-                    onClick={handleClose}
-                    sx={{ borderRadius: 0 }}
-                    aria-label={`Close ${tooltip}`}
-                >
-                    <CloseIcon />
-                </IconButton>
-                {children}
-            </Drawer>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<Tooltip title={tooltip}>
+				<IconButton
+					onClick={handleToggle}
+					sx={{ borderRadius: 1, width: "100%" }}
+					aria-label={ariaLabel}
+				>
+					{icon}
+				</IconButton>
+			</Tooltip>
+			<Drawer variant="persistent" anchor={anchor} open={open}>
+				<IconButton
+					onClick={handleClose}
+					sx={{ borderRadius: 0 }}
+					aria-label={`Close ${tooltip}`}
+				>
+					<CloseIcon />
+				</IconButton>
+				{children}
+			</Drawer>
+		</Fragment>
+	);
 };
