e111d6d5abde615bc306c432cbbff785742674a3
djohoe28 <djohoe28@gmail.com>
2025-04-08 20:40:38 +0300

    (DRAFT TODO) MemberSettings w/ MemberFormControl

--- src/components/Members/Write/MemberFormControl.tsx
+++ src/components/Members/Write/MemberFormControl.tsx
@@ -1,50 +1,73 @@
-import { Stack, InputLabel, TextField, Input, Tooltip, IconButton } from "@mui/material";
-import RestoreIcon from "@mui/icons-material/Restore";
+import { Stack, InputLabel, Input, Tooltip, IconButton, InputLabelTypeMap } from "@mui/material";
+import { HTMLInputTypeAttribute, useCallback, useEffect, useMemo, useState } from "react";
+import RestoreIcon from '@mui/icons-material/Restore';
+import { MemberModel } from "@models/App/Member.model";
 
-interface MemberFormControlProps {
-	label: string;
-	value: string;
-	placeholder?: string;
-	type?: "text" | "color";
-	isChanged: boolean;
-	onChange: (value: string) => void;
-	onReset: () => void;
+type FieldType = keyof Partial<Omit<MemberModel, "id">>
+
+type FieldMap = {
+	[key in FieldType]: HTMLInputTypeAttribute; // LINT: InputLabelTypeMap["props"]["children"] ?
 }
 
-export function MemberFormControl({ label, value, placeholder, type = "text", isChanged, onChange, onReset }: MemberFormControlProps) {
-	return (
-		<Stack direction="row" spacing={2} alignItems="center">
-			<InputLabel htmlFor={label.toLowerCase()}>{label}{isChanged ? "*" : ""}</InputLabel>
-			{type === "color" ? (
-				<Input
-					sx={{ flexGrow: 1 }}
-					name={label}
-					type="color"
-					value={value}
-					onChange={(e) => onChange(e.target.value)}
-				/>
-			) : (
-				<TextField
-					name={label}
-					placeholder={placeholder}
-					value={value}
-					onChange={(e) => onChange(e.target.value)}
-				/>
-			)}
-			<Tooltip title={`Restore ${label}`}>
-				<span>
-					{/** MUI: You are providing a disabled `button` child to the Tooltip component.
+// TODO: Leverage FIELD_MAP, or keep compartmentalized?
+const FIELD_MAP: FieldMap = {
+	name: "text",
+	color: "color",
+}
+
+interface MemberFormControlProps<T> {
+	fieldName: FieldType;
+	inputType: HTMLInputTypeAttribute;
+	documentValue?: T;
+	defaultValue: T;
+	sendMemberSettings: (settings: Partial<Omit<MemberModel, "id">>, merge?: boolean) => void;
+}
+
+export function MemberFormControl<T>({ documentValue, defaultValue, fieldName, inputType = FIELD_MAP[fieldName], sendMemberSettings }: MemberFormControlProps<T>) {
+	// States
+	const [value, setValue] = useState<T>(documentValue || defaultValue);
+	// Memos
+	const isChanged = useMemo(() => (documentValue || defaultValue) !== value, [documentValue, defaultValue, value]);
+	const capitalizedName = useMemo(() => fieldName.charAt(0).toUpperCase() + fieldName.slice(1), [fieldName]);
+	const tooltipText = useMemo(() => `Restore ${capitalizedName}`, [capitalizedName]);
+	// Callbacks
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(event.target.value as T); // TODO: Type assertion for value
+	};
+	const handleFieldReset = useCallback((send?: boolean) => {
+		setValue(documentValue || defaultValue);
+		if (send) sendMemberSettings({ [fieldName]: documentValue })
+	}, [documentValue, defaultValue, sendMemberSettings]);
+	const handleResetTrue = useCallback(() => handleFieldReset(true), [handleFieldReset]);
+	// Effects
+	useEffect(() => {
+		// If data has been updated from the server, update local state to match (default if undefined).
+		// TODO: Make sure this is correct.
+		setValue(documentValue || defaultValue);
+	}, [documentValue, defaultValue, setValue]);
+
+	return <Stack direction="row" spacing={2} alignItems="center">
+		<InputLabel htmlFor={fieldName}>{capitalizedName}{isChanged ? "*" : ""}</InputLabel>
+		<Input
+			sx={{ flexGrow: 1 }}
+			name={fieldName}
+			type={inputType}
+			value={value}
+			onChange={handleChange}
+		/>
+		<Tooltip title={tooltipText}>
+			<span>
+				{/** MUI: You are providing a disabled `button` child to the Tooltip component.
 A disabled element does not fire events.
 Tooltip needs to listen to the child element's events to display the title. */}
-					<IconButton
-						onClick={onReset}
-						aria-label={`Restore ${label}`}
-						disabled={!isChanged}
-					>
-						<RestoreIcon />
-					</IconButton>
-				</span>
-			</Tooltip>
-		</Stack>
-	);
-};
+				<IconButton
+					onClick={handleResetTrue}
+					aria-label={tooltipText}
+					disabled={!isChanged}
+				>
+					<RestoreIcon />
+				</IconButton>
+			</span>
+		</Tooltip>
+	</Stack>
+}
--- src/components/Members/Write/MemberSettings.tsx
+++ src/components/Members/Write/MemberSettings.tsx
@@ -1,8 +1,9 @@
+// MemberSettings.tsx
 import { RoomReferencesContext } from "@contexts/RoomReferencesContext";
 import { useMemberSettings } from "@hooks/useMemberSettings";
 import { Skeleton, Stack, Typography, Grid, Button, CircularProgress } from "@mui/material";
-import { stringToColor, DEFAULT_NAME } from "@utils/String.utils";
-import { useContext, useMemo, useState, useCallback, useEffect } from "react";
+import { stringToColor } from "@utils/String.utils";
+import { useContext, useMemo } from "react";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import { ErrorDisplay } from "@components/ErrorDisplay";
 import { MemberFormControl } from "./MemberFormControl";
@@ -20,95 +21,57 @@ export function MemberSettings() {
 	// Memos (Default Values)
 	const defaultName = useMemo(() => "", []); // TODO: Leverage `DEFAULT_NAME` (see String.utils.ts)
 	const defaultColor = useMemo(() => documentData ? stringToColor(documentData.id) : "", [documentData?.id]);
-	// States
-	const [name, setName] = useState<string>(documentData?.name || "");
-	const [color, setColor] = useState<string>(documentData?.color || defaultColor);
-	// Memos (Derived States)
-	const isChangedColor = useMemo(() => (documentData?.color || defaultColor) !== color, [documentData?.color, color]);
-	const isChangedName = useMemo(() => (documentData?.name || "") !== name, [documentData?.name, name]);
-	// Callbacks
-	const handleNameReset = useCallback((send?: boolean) => {
-		setName(documentData?.name || defaultName);
-		if (send) sendMemberSettings({ name: documentData?.name });
-	}, [documentData?.name, sendMemberSettings]);
-	const handleColorReset = useCallback((send?: boolean) => {
-		setColor(documentData?.color || defaultColor);
-		if (send) sendMemberSettings({ color: documentData?.color });
-	}, [documentData?.color, sendMemberSettings, defaultColor]);
-	const handleReset = useCallback(() => {
-		// Reset each local state to default if respective server data is undefined.
-		// NOTE: If server data *is* defined for the state, it will be updated in the useEffect below.
-		if (!documentData?.name) setName(defaultName);
-		if (!documentData?.color) setColor(defaultColor);
-		sendMemberSettings({}, false);
-	}, [sendMemberSettings, documentData, defaultColor]);
-	// Effects
-	useEffect(() => {
-		// If data has been updated from the server, update local state to match (default if undefined).
-		if (documentData) {
-			setName(documentData.name || defaultName);
-			setColor(documentData.color || defaultColor);
-		}
-	}, [documentData, defaultColor]);
 	// FEATURE: Profile Image?
 
 	// TODO: Error order? Return both?
 	if (documentError) return <ErrorDisplay error={documentError} />;
 	if (hookError) return <ErrorDisplay error={hookError} />;
-	return (documentLoading || hookLoading) ? <Skeleton /> : (
-		<Stack
-			component="form"
-			spacing={2}
-			onSubmit={(event) => event.preventDefault()}
-			sx={{ p: 2 }}
-		>
-			<Typography variant="h6" component="h3" sx={{ mb: 2 }}>
-				Member Settings
-			</Typography>
-			<Typography variant="body2" component="p" sx={{ mb: 2 }}>
-				ID: {documentData?.id}
-			</Typography>
-			<MemberFormControl
-				label="Name"
-				value={name}
-				placeholder={DEFAULT_NAME}
-				isChanged={isChangedName}
-				onChange={setName}
-				onReset={() => handleNameReset(true)}
-			/>
-			<MemberFormControl
-				label="Color"
-				value={color}
-				type="color"
-				isChanged={isChangedColor}
-				onChange={setColor}
-				onReset={() => handleColorReset(true)}
-			/>
-			<Grid container textAlign="center">
-				<Grid size="grow">
-					<Button
-						type="reset"
-						onClick={(e) => {
-							e.preventDefault();
-							handleReset();
-						}}
-					>
-						Reset
-					</Button>
-				</Grid>
-				<Grid size="grow">
-					{hookSending ? (
-						<CircularProgress />
-					) : (
-						<Button
-							type="submit"
-							onClick={() => sendMemberSettings({ name, color }, false)}
-						>
-							Save
-						</Button>
-					)}
-				</Grid>
+	return (documentLoading || hookLoading) ? <Skeleton /> : <Stack
+		component="form"
+		spacing={2}
+		action={(formData) => {
+			// TODO: Streamline this.
+			const name = !formData.get("name") ? undefined : formData.get("name") as string;
+			const color = !formData.get("color") ? undefined : formData.get("color") as string;
+			const data = { name: name, color: color };
+			sendMemberSettings(data, false);
+		}}
+		onReset={(event) => { console.log(event); sendMemberSettings({}, false); }}
+		sx={{ p: 2 }}
+	>
+		<Typography variant="h6" component="h3" sx={{ mb: 2 }}>
+			Member Settings
+		</Typography>
+		<Typography variant="body2" component="p" sx={{ mb: 2 }}>
+			ID: {documentData?.id || "N/A"}
+		</Typography>
+		<MemberFormControl
+			fieldName="name"
+			inputType="text"
+			defaultValue={defaultName}
+			documentValue={documentData?.name}
+			sendMemberSettings={sendMemberSettings}
+		/>
+		<MemberFormControl
+			fieldName="color"
+			inputType="color"
+			defaultValue={defaultColor}
+			documentValue={documentData?.color}
+			sendMemberSettings={sendMemberSettings}
+		/>
+		<Grid container textAlign='center'>
+			<Grid size='grow'>
+				<Button type='reset'>Reset</Button>
 			</Grid>
-		</Stack>
-	);
+			<Grid size='grow'>
+				{
+					// NOTE: Box wrapper to force CircularProgress alignItems='center' without breaking color input.
+					hookSending
+						? <CircularProgress />
+						: <Button type="submit">Submit</Button>
+				}
+			</Grid>
+		</Grid>
+
+	</Stack>
 }
