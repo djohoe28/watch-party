ae7aa1e6f5e297b4075574abfe978961fa5f393a
djohoe28 <djohoe28@gmail.com>
2025-04-08 18:57:21 +0300

    (DRAFT) Added MemberFormControl to MemberSettings.

--- src/components/Members/Write/MemberSettings.tsx
+++ src/components/Members/Write/MemberSettings.tsx
@@ -1,11 +1,11 @@
 import { RoomReferencesContext } from "@contexts/RoomReferencesContext";
 import { useMemberSettings } from "@hooks/useMemberSettings";
-import { Skeleton, Stack, Typography, InputLabel, TextField, Tooltip, IconButton, Input, Grid, Button, CircularProgress } from "@mui/material";
+import { Skeleton, Stack, Typography, Grid, Button, CircularProgress } from "@mui/material";
 import { stringToColor, DEFAULT_NAME } from "@utils/String.utils";
 import { useContext, useMemo, useState, useCallback, useEffect } from "react";
 import { useDocumentData } from "react-firebase-hooks/firestore";
-import RestoreIcon from '@mui/icons-material/Restore';
 import { ErrorDisplay } from "@components/ErrorDisplay";
+import { MemberFormControl } from "./MemberFormControl";
 
 export function MemberSettings() {
 	// Contexts
@@ -55,88 +55,60 @@ export function MemberSettings() {
 	// TODO: Error order? Return both?
 	if (documentError) return <ErrorDisplay error={documentError} />;
 	if (hookError) return <ErrorDisplay error={hookError} />;
-	return (documentLoading || hookLoading) ? <Skeleton /> : <Stack
-		component="form"
-		spacing={2}
-		onSubmit={(event) => event.preventDefault()}
-		sx={{ p: 2 }}
-	>
-		<Typography variant="h6" component="h3" sx={{ mb: 2 }}>
-			Member Settings
-		</Typography>
-		<Typography variant="body2" component="p" sx={{ mb: 2 }}>
-			ID: {documentData?.id}
-		</Typography>
-		<Stack direction="row" spacing={2} alignItems='center'>
-			<InputLabel htmlFor="name">Name{isChangedName ? "*" : ""}</InputLabel>
-			<TextField
-				name="Name"
-				placeholder={DEFAULT_NAME}
+	return (documentLoading || hookLoading) ? <Skeleton /> : (
+		<Stack
+			component="form"
+			spacing={2}
+			onSubmit={(event) => event.preventDefault()}
+			sx={{ p: 2 }}
+		>
+			<Typography variant="h6" component="h3" sx={{ mb: 2 }}>
+				Member Settings
+			</Typography>
+			<Typography variant="body2" component="p" sx={{ mb: 2 }}>
+				ID: {documentData?.id}
+			</Typography>
+			<MemberFormControl
+				label="Name"
 				value={name}
-				onChange={(e) => setName(e.target.value)}
+				placeholder={DEFAULT_NAME}
+				isChanged={isChangedName}
+				onChange={setName}
+				onReset={() => handleNameReset(true)}
 			/>
-			<Tooltip title="Restore Name">
-				<span>
-					{/** MUI: You are providing a disabled `button` child to the Tooltip component.
-A disabled element does not fire events.
-Tooltip needs to listen to the child element's events to display the title. */}
-					<IconButton
-						onClick={() => handleNameReset(true)}
-						aria-label="Restore Name"
-						disabled={!isChangedName}
-					>
-						<RestoreIcon />
-					</IconButton>
-				</span>
-			</Tooltip>
-		</Stack>
-		<Stack direction="row" spacing={2} alignItems="center">
-			<InputLabel htmlFor="color">Color{isChangedColor ? "*" : ""}</InputLabel>
-			<Input
-				sx={{ flexGrow: 1 }}
-				name="Color"
-				type="color"
+			<MemberFormControl
+				label="Color"
 				value={color}
-				onChange={(e) => setColor(e.target.value)}
+				type="color"
+				isChanged={isChangedColor}
+				onChange={setColor}
+				onReset={() => handleColorReset(true)}
 			/>
-			<Tooltip title="Restore Color">
-				<span>
-					{/** MUI: You are providing a disabled `button` child to the Tooltip component.
-A disabled element does not fire events.
-Tooltip needs to listen to the child element's events to display the title. */}
-					<IconButton
-						onClick={() => handleColorReset(true)}
-						aria-label="Restore Color"
-						disabled={!isChangedColor}
+			<Grid container textAlign="center">
+				<Grid size="grow">
+					<Button
+						type="reset"
+						onClick={(e) => {
+							e.preventDefault();
+							handleReset();
+						}}
 					>
-						<RestoreIcon />
-					</IconButton>
-				</span>
-			</Tooltip>
-		</Stack>
-		<Grid container textAlign='center'>
-			<Grid size='grow'>
-				<Button
-					type='reset'
-					onClick={(e) => { e.preventDefault(); handleReset(); }}
-				>
-					Reset
-				</Button>
-			</Grid>
-			<Grid size='grow'>
-				{
-					// NOTE: Box wrapper to force CircularProgress alignItems='center' without breaking color input.
-					hookSending
-						? <CircularProgress />
-						: <Button
+						Reset
+					</Button>
+				</Grid>
+				<Grid size="grow">
+					{hookSending ? (
+						<CircularProgress />
+					) : (
+						<Button
 							type="submit"
 							onClick={() => sendMemberSettings({ name, color }, false)}
 						>
 							Save
 						</Button>
-				}
+					)}
+				</Grid>
 			</Grid>
-		</Grid>
-
-	</Stack>
+		</Stack>
+	);
 }
