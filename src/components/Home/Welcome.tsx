8ef0ceede6ad585952c978f2474cb4a9e401c8fc
djohoe28 <djohoe28@gmail.com>
2025-04-08 18:05:21 +0300

    (DRAFT) Added Welcome component.

---
+++ src/components/Home/Welcome.tsx
@@ -0,0 +1,10 @@
+import { AuthContext } from "@contexts/AuthContext";
+import { Typography } from "@mui/material";
+import { useContext } from "react";
+
+export function Welcome() {
+	const authContext = useContext(AuthContext);
+	return <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
+			Welcome, {authContext?.payload?.isAnonymous ? "Anonymous" : authContext?.payload?.displayName}!
+		</Typography>
+}
