a18d5dc0a42a9beab90629a7a929cd11c4f92159
djohoe28 <djohoe28@gmail.com>
2025-04-08 18:27:26 +0300

    Compartmentalized MembersArea with MemberControls.

--- src/components/Members/Read/MembersArea.tsx
+++ src/components/Members/Read/MembersArea.tsx
@@ -1,14 +1,11 @@
-import { DrawerWithToggle } from "@components/DrawerWithToggle";
 import { MessageList } from "@components/Messages/Read/MessageList";
 import { MessageField } from "@components/Messages/Write/MessageField";
 import { RoomReferencesContext } from "@contexts/RoomReferencesContext";
-import { Stack, Grid } from "@mui/material";
+import { Stack } from "@mui/material";
 import { useContext, useMemo } from "react";
-import { MemberSettings } from "../Write/MemberSettings";
-import { MembersList } from "./MembersList";
 import { MembersProvider } from "@components/Providers/MembersProvider";
-import SettingsIcon from '@mui/icons-material/Settings';
-import PeopleIcon from '@mui/icons-material/People';
+import { MemberControls } from "./MemberControls";
+
 
 export function MembersArea() {
 	// Contexts
@@ -18,29 +15,7 @@ export function MembersArea() {
 
 	return <Stack direction="column" spacing={2}>
 		<MembersProvider membersRef={membersRef}>
-			<Grid container spacing={2} textAlign='center'>
-				<Grid size='grow'>
-					<DrawerWithToggle
-						icon={<SettingsIcon />}
-						tooltip="Member Settings"
-						anchor="left"
-						ariaLabel="Toggle Member Settings"
-					>
-						<MemberSettings />
-					</DrawerWithToggle>
-
-				</Grid>
-				<Grid size='grow'>
-					<DrawerWithToggle
-						icon={<PeopleIcon />}
-						tooltip="Members List"
-						anchor="right"
-						ariaLabel="Toggle Members List"
-					>
-						<MembersList />
-					</DrawerWithToggle>
-				</Grid>
-			</Grid>
+			<MemberControls />
 			<MessageList />
 		</MembersProvider>
 		<MessageField />
