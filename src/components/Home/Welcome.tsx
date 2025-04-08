ca3f63cd5c155e1ae04ee74fc490bca2ca03bd67
djohoe28 <djohoe28@gmail.com>
2025-04-08 18:10:51 +0300

    Updated Welcome to leverage loading, error.

--- src/components/Home/Welcome.tsx
+++ src/components/Home/Welcome.tsx
@@ -1,10 +1,11 @@
 import { AuthContext } from "@contexts/AuthContext";
-import { Typography } from "@mui/material";
+import { Alert, Skeleton, Typography } from "@mui/material";
 import { useContext } from "react";
 
 export function Welcome() {
 	const authContext = useContext(AuthContext);
-	return <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
-			Welcome, {!authContext.payload ? "Guest" : authContext?.payload?.isAnonymous ? "Anonymous" : authContext?.payload?.displayName}!
+	if(!authContext) return <Alert severity="error">Failed to authorize User.</Alert>;
+	return authContext.loading ? <Skeleton /> : <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
+			Welcome, {authContext?.payload?.isAnonymous ? "Anonymous" : authContext?.payload?.displayName}!
 		</Typography>
 }
