d29a04bca69b68111c6f83abb2180f666e544858
djohoe28 <djohoe28@gmail.com>
2025-04-09 19:20:43 +0300

    Added FileInputButton (modified from MUI).

---
+++ src/components/Media/Write/FileInputButton.tsx
@@ -0,0 +1,38 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+
+// SEE: https://mui.com/material-ui/react-button/#file-upload
+
+const VisuallyHiddenInput = styled('input')({
+	clip: 'rect(0 0 0 0)',
+	clipPath: 'inset(50%)',
+	height: 1,
+	overflow: 'hidden',
+	position: 'absolute',
+	bottom: 0,
+	left: 0,
+	whiteSpace: 'nowrap',
+	width: 1,
+});
+
+export default function FileInputButton() {
+	return (
+		<Button
+			component="label"
+			role={undefined}
+			variant="contained"
+			tabIndex={-1}
+			startIcon={<UploadFileIcon />}
+		>
+			Upload media
+			<VisuallyHiddenInput
+				type="file"
+				onChange={(event) => console.log(event.target.files)}
+				// TODO: Route event.target.files to the appropriate context.
+				// FEATURE: multiple
+				accept="image/*,video/*,audio/*" // TODO: Does ReactPlayer support images?
+			/>
+		</Button>
+	);
+}
