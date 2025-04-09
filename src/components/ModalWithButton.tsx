f3dc7c661493e44c73de52ae19cc5210268d5045
djohoe28 <djohoe28@gmail.com>
2025-04-09 18:54:39 +0300

    Added ModalWithButton.

---
+++ src/components/ModalWithButton.tsx
@@ -0,0 +1,31 @@
+import { Box, Tooltip, IconButton, Modal } from "@mui/material";
+import { useState, useCallback, ReactNode, ReactElement } from "react";
+
+export interface ModalWithButtonProps {
+	icon: ReactNode;
+	tooltip: string;
+	children: ReactElement;
+	ariaLabel: string;
+}
+
+export function ModalWithButton({ icon, tooltip, children, ariaLabel }: ModalWithButtonProps) {
+	// State
+	const [open, setOpen] = useState(false);
+	// Callbacks
+	const handleToggle = useCallback(() => setOpen((open) => !open), []);
+	const handleClose = useCallback(() => setOpen(false), []);
+
+	return <Box>
+		<Tooltip title={tooltip}>
+			<IconButton
+				onClick={handleToggle}
+				sx={{ borderRadius: 1, width: "100%" }}
+				aria-label={ariaLabel}
+				children={icon}
+			/>
+		</Tooltip>
+		<Modal open={open} onClose={handleClose}>
+			{children}
+		</Modal>
+	</Box>
+}
