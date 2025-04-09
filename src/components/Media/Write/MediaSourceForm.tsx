e986e99d7215a2c07bd42ecc4a9b5d9784d3a9d6
djohoe28 <djohoe28@gmail.com>
2025-04-09 18:54:46 +0300

    (PLACEHOLDER) Added MediaSourceForm.

---
+++ src/components/Media/Write/MediaSourceForm.tsx
@@ -0,0 +1,36 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+const style = {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	bgcolor: 'background.paper',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
+  };
+
+export function MediaSourceForm() {
+	// Contexts
+	// Memos (Derived Contexts)
+	// Hooks
+
+	return <Stack
+		component="form"
+		spacing={2}
+		action={(formData) => console.log(formData)}
+		onReset={(event) => console.log(event)}
+		sx={{ p: 2 }}
+	>
+		<Box sx={style}>
+			<Typography id="modal-modal-title" variant="h6" component="h2">
+				Text in a modal
+			</Typography>
+			<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+				Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+			</Typography>
+		</Box>
+	</Stack>;
+}
