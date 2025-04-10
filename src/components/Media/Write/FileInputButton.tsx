b5843d9e8ed435471132bbe508e400613b6b6943
djohoe28 <djohoe28@gmail.com>
2025-04-10 17:53:40 +0300

    Implemented VisuallyHiddenInput in FileInputButton. + Also, added FormHelperText with file name state.

--- src/components/Media/Write/FileInputButton.tsx
+++ src/components/Media/Write/FileInputButton.tsx
@@ -1,39 +1,38 @@
-import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { FormControl, FormHelperText } from '@mui/material';
+import { VisuallyHiddenInput } from '@components/VisuallyHiddenInput';
+import { useState } from 'react';
 
 // SEE: https://mui.com/material-ui/react-button/#file-upload
 
-const VisuallyHiddenInput = styled('input')({
-	clip: 'rect(0 0 0 0)',
-	clipPath: 'inset(50%)',
-	height: 1,
-	overflow: 'hidden',
-	position: 'absolute',
-	bottom: 0,
-	left: 0,
-	whiteSpace: 'nowrap',
-	width: 1,
-});
-
 export default function FileInputButton({ onChange, name }: { onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void, name?: string }) {
+	const [fileName, setFileName] = useState<string>();
+
 	return (
-		<Button
-			component="label"
-			role={undefined}
-			variant="contained"
-			tabIndex={-1}
-			startIcon={<UploadFileIcon />}
-		>
-			Upload media
-			<VisuallyHiddenInput
-				type="file"
-				onChange={onChange}
-				name={name}
-				// TODO: Route event.target.files to the appropriate context.
-				// FEATURE: multiple
-				accept="image/*,video/*,audio/*" // TODO: Does ReactPlayer support images?
-			/>
-		</Button>
+		<FormControl>
+			<Button
+				component="label"
+				role={undefined}
+				variant="contained"
+				tabIndex={-1}
+				startIcon={<UploadFileIcon />}
+			>
+				Upload media
+				<VisuallyHiddenInput
+					type="file"
+					onChange={(event) => {
+						// LINT: Seems clunky (double state?)
+						setFileName(event.target.files?.item(0)?.name);
+						if (onChange) onChange(event);
+					}}
+					name={name}
+					// TODO: Route event.target.files to the appropriate context.
+					// FEATURE: multiple
+					accept="image/*,video/*,audio/*" // TODO: Does ReactPlayer support images?
+				/>
+			</Button>
+			<FormHelperText children={fileName ?? "No file loaded."} />
+		</FormControl>
 	);
 }
