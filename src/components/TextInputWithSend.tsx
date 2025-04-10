fddbbd46e725d028e0acb5b131ac2b296589e34b
djohoe28 <djohoe28@gmail.com>
2025-04-10 17:58:49 +0300

    Refactored TextInputWithSend as simple TextField.

--- src/components/TextInputWithSend.tsx
+++ src/components/TextInputWithSend.tsx
@@ -1,29 +1,22 @@
-import { FormControl, InputLabel, InputAdornment, IconButton, Input, FormHelperText } from "@mui/material";
+import { InputAdornment, IconButton, TextField, TextFieldVariants, TextFieldProps } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 
-export function TextInputWithSend({ labelText, name, helperText }: { labelText?: string, name?: string, helperText?: string }) {
-	return <FormControl>
-		<InputLabel
-			// htmlFor="filled-adornment-password"
-			children={labelText}
-		/>
-		<Input
-			// id="filled-adornment-password"
-			name={name}
-			type="text"
-			endAdornment={
-				<InputAdornment position="end">
-					<IconButton
-						children={<SendIcon />}
-						edge="end"
-						aria-label="Send"
-						type="submit"
-					/>
-				</InputAdornment>
+export function TextInputWithSend<Variant extends TextFieldVariants>(props: { variant?: Variant } & Omit<TextFieldProps, "variant">) {
+	return <TextField
+		slotProps={{
+			input: {
+				endAdornment:
+					<InputAdornment position="end">
+						<IconButton
+							type="submit"
+							edge="end"
+							aria-label="Send"
+						>
+							<SendIcon />
+						</IconButton>
+					</InputAdornment>
 			}
-		/>
-		{helperText
-			? <FormHelperText children={helperText} />
-			: null}
-	</FormControl>
+		}}
+		{...props}
+	/>;
 }
