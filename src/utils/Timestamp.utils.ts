7d458d050432eb109a6475b0031436cb9d59478b
djohoe28 <djohoe28@gmail.com>
2025-04-12 19:14:09 +0300

    Added Timestamp.utils.ts

---
+++ src/utils/Timestamp.utils.ts
@@ -0,0 +1,26 @@
+import { Timestamp } from "firebase/firestore";
+
+export function toDisplayString(timestamp: number | Date | Timestamp): string {
+	let date: Date;
+	if (timestamp instanceof Date) {
+		date = timestamp;
+	} else if (typeof timestamp === "number") {
+		date = new Date(timestamp);
+	} else {
+		date = timestamp.toDate();
+	}
+	return date.toISOString().replace("T", " ").substring(0, 19);
+}
+
+export function toTimespanString(
+	seconds: number | undefined | null,
+	showHours = false
+): string {
+	if (seconds === null || seconds === undefined) {
+		// LINT: `!seconds` catches `seconds=0`...
+		return showHours ? "??:??:??" : "??:??";
+	}
+	return new Date(seconds * 1000)
+		.toISOString()
+		.slice(seconds > 3600 || showHours ? 11 : 14, 19);
+}
