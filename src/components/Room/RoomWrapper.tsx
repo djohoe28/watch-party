66f04577b74443ebf219ec8cff3a93778fec5123
djohoe28 <djohoe28@gmail.com>
2025-04-12 19:29:38 +0300

    Added roomId param check to RoomWrapper.

--- src/components/Room/RoomWrapper.tsx
+++ src/components/Room/RoomWrapper.tsx
@@ -1,9 +1,11 @@
+import { ErrorDisplay } from "@components/ErrorDisplay";
+import { Room } from "@components/Room/Room";
 import { useParams } from "react-router";
-import { Room } from "./Room";
 
 export function RoomWrapper() {
 	// Hooks
 	const params = useParams();
 
-	return <Room roomId={params.roomId!} />; // TODO LINTODO: Validate params.roomId
+	if (!params.roomId) return <ErrorDisplay error={new Error("Room ID not found")} />;
+	return <Room roomId={params.roomId} />;
 }
