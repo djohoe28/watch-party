02676c6c78caa4a768c3df9dd51c8828885673e9
djohoe28 <djohoe28@gmail.com>
2025-04-13 17:33:18 +0300

    Removed bad ModalBox style(s).

--- src/components/Utilities/ModalBox.tsx
+++ src/components/Utilities/ModalBox.tsx
@@ -8,11 +8,12 @@ export function ModalBox({ children }: { children: ReactNode }) {
 			top: '50%',
 			left: '50%',
 			transform: 'translate(-50%, -50%)',
-			width: 400,
+			// width: 800,
 			bgcolor: 'background.paper',
-			border: '2px solid #000',
+			// border: '2px solid #000',
 			boxShadow: 24,
 			p: 4,
-		}}
-		children={children} />
+		}}>
+		{children}
+	</Box>
 }
