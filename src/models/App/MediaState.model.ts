04ef45ccc01a7892f9866404860f3f610f13186e
djohoe28 <djohoe28@gmail.com>
2025-04-14 20:25:59 +0300

    Sorted MediaState fields by importance.

--- src/models/App/MediaState.model.ts
+++ src/models/App/MediaState.model.ts
@@ -1,11 +1,11 @@
 import { Timestamp } from "firebase/firestore";
 
 export interface MediaState {
-	currentTime: number;
-	description: string;
-	duration: number;
-	isFile: boolean;
-	isPaused: boolean;
-	lastUpdated: Timestamp | null; // TODO: Snapshot Options type safety.
 	src: string;
+	isPaused: boolean;
+	currentTime: number;
+	duration: number;
+	description: string;
+	isFile: boolean;
+	lastUpdated: Timestamp | null; // TODO: Snapshot Options type safety.
 }
